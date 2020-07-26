01a6a0a5d0289a4fd50a484843087d491ef0bd7b
b <a>
2020-07-26 20:15:40 +1000

    commit3

--- src/AI.hs
+++ src/AI.hs
@@ -24,32 +24,37 @@ ais :: [(String, AIFunc)]
 ais = [ ("firstLegalMove", NoLookahead firstLegalMove), ("protoType", NoLookahead protoType)
       ]
 
+firstLegalMove :: GameState -> Move
+firstLegalMove st = head (legalMoves st)
+
+protoType :: GameState -> Move
+protoType st = head (moveSelect 3 (legalMoves st))
+
 -- | A very simple AI, which picks the first move returned by the
 -- 'legalMoves' function. AIs can rely on the 'legalMoves' list being
 -- non-empty; if there were no legal moves, the framework would have
 -- ended the game.
 
-{-|##################
+caclHeur :: GameState -> Int
+caclHeur st = fst (countPieces st) - snd (countPieces st) + (squareValue st)
 
-locatoin1 :: Location
-locatoin1 = Location 3 4
-move1 :: Move
-move1 = Move locatoin1 locatoin1
+sumList :: [a]
 
-calcMoveValue :: GameState -> Move
-calcMoveValue _ = move1
-heur :: GameState -> Int
-heur _ = 0
+squareValue :: GameState -> Int
+squareValue st = foldl' count (0,0) (concat (board st))
+  where
+    count :: (Int, Int) -> Square -> (Int, Int)
+    count (p1,p2) sq = case sq of
+        Piece Player1 ->
 
--}
+squareValue ::
+--minMax :: Int -> [Move] -> Int
+--minMax acc (x:xs) =
+
+moveValue :: Move -> Int
+moveValue m = 1
 
 moveSelect :: Integer -> [Move] -> [Move]
 moveSelect _ [] = []
 moveSelect 1 (x:xs) = x:[]
 moveSelect n (x:xs) = moveSelect (n-1) xs
-
-firstLegalMove :: GameState -> Move
-firstLegalMove st = head (moveSelect 3 (legalMoves st))
-
-protoType :: GameState -> Move
-protoType st = head (moveSelect 2 (legalMoves st))
